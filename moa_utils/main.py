ab1bcdd698d4a8149b0c4a90c6994040b15628bc
Andy Cheung <andychinka@gmail.com>
2020-10-11 17:51:48 +0800

    using repeater for fold

--- moa_utils/main.py
+++ moa_utils/main.py
@@ -119,7 +119,8 @@ def set_cols(folds, test, target):
 def run_training(c):
     # fold, seed
     seed = c["seed"]
-    fold = c["fold"]
+    fold = tune.suggest.repeater.TRIAL_INDEX #c["fold"]
+    print("TRIAL_INDEX: ", fold)
     # folds = c["folds"]
     # test = c["test"]
     # target = c["target"]
